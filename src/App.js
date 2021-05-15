3e54c4bc83cad81a3b1ad8a2ed4c539166a8d3a0
doballa <dennisobala@gmail.com>
2021-05-15 23:53:20 +0300

    Adding a card and search bar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
-  comp
+
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters:users }));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
   }
   render() {
-  return (
-    <div className="App">
-      <CardList monsters={this.state.monsters}>
-        
-      </CardList>     
-    </div>
-  );
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search-monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
   }
 }
 
